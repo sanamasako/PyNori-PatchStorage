df264da74ed09f776676ddab7a08a7b17bf1979e
Shirley-XML <53914467+sanamasako@users.noreply.github.com>
2025-11-12 03:31:16 -0500

    Delete patches/2.3.0/patch-11-11-2025.py

--- patches/2.3.0/patch-11-11-2025.py
+++
@@ -1,3 +0,0 @@
-def ShadoWip(): # I changed the comment for the funny!
-	typewriter_shado("Wow, you're so talented that I've... run out of attacks???"); time.sleep(2); typewriter_shado("How interesting. Oh well.")
-	while True: continue
