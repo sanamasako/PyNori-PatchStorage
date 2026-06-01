abf2ae41d64406790d1d584df80d0b136121031a
Shirley-XML <53914467+sanamasako@users.noreply.github.com>
2026-05-31 20:58:48 -0400

    Upload patch-5-31-2026.py.

---
+++ patches/2.4.0/patch-5-31-2026.py
@@ -0,0 +1,16 @@
+def ProgressDestroyer(): # for the Demotion item
+	global location, steps, LocCode, ToneCollectedYet, debug, DualQMode, ActiveQuirk, Quirk1, Quirk2, LocVisits, Advance, tef; ToneCollectedYet = False; Log("Backtracking time!")
+	try: print(Fore.YELLOW+"DEBUG: "+Fore.WHITE+f"Location: {location}, Steps Taken: {steps}"); print(Fore.YELLOW+f"DEBUG: {Fore.WHITE}ActiveQuirk: {ActiveQuirk}" if not DualQMode else Fore.YELLOW+f"DEBUG: {Fore.WHITE}Quirk1: {Quirk1}, Quirk2: {Quirk2}")
+	except ValueError:
+		SoundCh.SFX.play(tef)
+		if args.debug: debugFail()
+	if "Randomiser" in [ActiveQuirk, Quirk1, Quirk2]: ProgressMaker(Demotion=True) # the randomisation is no different so just do the normal thing
+	else:
+		if location == "Kiku Village": print("... nothing changed."); pause()
+		elif location == "Tanpopo Town": location, LocCode = "Kiku Village", "Kiku"
+		elif location == "Nodium Valley": location, LocCode = "Tanpopo Town", "Tanpopo"
+		elif location == "Crystalline Oceanfront": location, LocCode = "Nodium Valley", "Nodium"
+		elif location == "Archlekia": location, LocCode = "Crystalline Oceanfront", "Ocean"
+		elif location == "Tanggal Volcano": location, LocCode = "Archlekia", "Archlekia"
+		elif location == "Dyarix": location, LocCode = "Tanggal Volcano", "Tanggal"
+	Advance = False; steps = 0; Log("Progress has NOT been made! Hell yeah!"); gameThing()
